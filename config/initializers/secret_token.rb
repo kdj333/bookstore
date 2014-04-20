76f733e0c53d756e326fe27ee14b602a95e232ad
vagrant <vagrant@kellogg-dev-box>
2014-04-20 01:03:11 +0000

    Generate the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookstore::Application.config.secret_key_base = '407229927664df9ef68cd1de818ec08c2603e1698a3c6080ac8c55a224ea9703b7a0f9f2f5d17d4d4c07ee9a4a9a30dcb1c2857da75e82049d02b7e9cdb4fb5b'
